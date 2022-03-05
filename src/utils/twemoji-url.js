487f2c027d1408a8f2fe3549033cffb2995280e6
datsukan <s.datsukan@gmail.com>
2022-03-06 06:23:59 +0900

    :wrench: fix metaタグを修正

--- src/utils/twemoji-url.js
+++ src/utils/twemoji-url.js
@@ -2,7 +2,7 @@ import twemoji from "twemoji"
 
 export function twemojiURL(emoji) {
   const codePoint = twemoji.convert.toCodePoint(emoji)
-  const baseUrl = "https://twemoji.maxcdn.com/v/latest/png/"
+  const baseUrl = "https://twemoji.maxcdn.com/v/latest/72x72/"
   const url = `${baseUrl}${codePoint.split("-")[0]}.png`
 
   return url
