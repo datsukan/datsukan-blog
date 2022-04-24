0bcc55c653bfb9b26485d57cd14a742d1870ab06
datsukan <s.datsukan@gmail.com>
2022-04-25 02:38:24 +0900

    :bug: fix リンクカードの表示不具合の改修

--- src/components/markdown/SkeletonLinkCard.tsx
+++ src/components/markdown/SkeletonLinkCard.tsx
@@ -7,17 +7,17 @@ export const SkeletonLinkCard = ({ className = "" }: Props) => {
     <div
       className={`h-32 flex animate-pulse overflow-hidden rounded-lg border border-gray-300 ${className}`}
     >
-      <div className="flex flex-1 flex-col justify-between p-3">
-        {/* <div>
+      {/* <div className="flex flex-1 flex-col justify-between p-3">
+        <div>
           <div className="!m-0 h-12 rounded-md bg-gray-200"></div>
           <div className="!mt-2 !mb-0 h-4 rounded-md bg-gray-200"></div>
         </div>
         <div className="flex items-center gap-2">
           <span className="h-5 w-5 rounded-full bg-gray-200"></span>
           <span className="h-4 w-24 rounded-md bg-gray-200"></span>
-        </div> */}
+        </div>
       </div>
-      <div className="aspect-square h-full bg-gray-200"></div>
+      <div className="aspect-square h-full bg-gray-200"></div> */}
     </div>
   )
 }
