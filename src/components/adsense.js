29647e2ff303b6518a3806f22a2062cfd5c89b31
datsukan <s.datsukan@gmail.com>
2023-01-04 01:27:29 +0900

    広告表示を修正

--- src/components/adsense.js
+++ src/components/adsense.js
@@ -7,7 +7,7 @@ export const Adsense = ({ className, path }) => {
 
   return (
     <ins
-      className={`adsbygoogle ${className}`}
+      className={`adsbygoogle block w-full ${className}`}
       data-ad-client="ca-pub-4435913134683646"
       data-ad-slot="5844173870"
       data-ad-format="auto"
